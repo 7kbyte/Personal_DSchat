e2ca69c3b8de536f6ad0a82b49045ddbaad51a4b
7kb <online_wlq@foxmail.com>
2026-06-07 13:30:55 +0800

    v2.3

--- config.py
+++ config.py
@@ -125,7 +125,7 @@ def get_assets_dir() -> str:
 
 # ==================== 窗口尺寸 ====================
 TITLE = "💬 DeepSeek Chat"
-APP_VERSION = "v2.2"
+APP_VERSION = "v2.3"
 
 WIN_WIDTH  = 1280
 WIN_HEIGHT = 840
